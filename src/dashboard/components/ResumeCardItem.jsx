da9cb90cc2f049dbe91456fe19ee4f4603d08f78
Arpit Rai <arpitr2002@outlook.com>
2024-10-30 03:53:31 +0530

    Error fix

--- src/dashboard/components/ResumeCardItem.jsx
+++ src/dashboard/components/ResumeCardItem.jsx
@@ -69,7 +69,7 @@ function ResumeCardItem({ resume, refreshData }) {
           <DropdownMenuContent>
             <DropdownMenuItem onClick={() => navigation('/dashboard/resume/' + resume.documentId + "/edit")}>Edit</DropdownMenuItem>
             <DropdownMenuItem onClick={() => navigation('/my-resume/' + resume.documentId + "/view")}>View</DropdownMenuItem>
-            <DropdownMenuItem onClick={() => navigation('/my-resume/' + resume.documentId + "/download")}>Download</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => navigation('/my-resume/' + resume.documentId + "/view")}>Download</DropdownMenuItem>
             <DropdownMenuItem onClick={() => setOpenAlert(true)}>Delete</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
